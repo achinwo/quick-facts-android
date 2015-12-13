d72673fe0711f6a4b13419eec931e20c5f4efe53
Anthony Chinwo <anthony.chinwo@gmail.com>
2015-12-13 21:00:27 +0000

    dummy list view

---
+++ app/src/main/java/com/aetoslabs/quickfacts/MainActivity.java
@@ -0,0 +1,31 @@
+package com.aetoslabs.quickfacts;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.aetoslabs.quickfacts.dummy.DummyContent;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+}
